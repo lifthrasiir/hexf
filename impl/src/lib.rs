bb6442c05644cb0e0b2cef798d92b5c5b9a12f91
Pat Hickey <pat@moreproductive.org>
2020-09-17 10:39:49 -0700

    use Rust 2018 and built-in support for expression proc macros
    Rust 1.45 and up supports expression proc macros, so the proc_macro_hack
    library that was previously required is no longer necessary.
    
    Additionally, update each crate to use Rust 2018, which means we can
    explicitly import macros via `use hexf::{hexf32, hexf64};`, rather than
    by `#[macro_use] extern crate hexf;`.
    
    Fix warnings around deprecated triple-elipsis `...`, the replacement
    syntax for inclusive ranges is `..=`.
    
    Uses the latest `syn` crate, which is now in the semver stable 1.0.x
    series.
    
    rustfmted with rustfmt 1.4.18 (ships with rust 1.46). Apoligies that
    this makes the diff harder to read, my editor did this automatically.

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -1,30 +1,43 @@
 //! Support library for `hexf`. Do not use directly.
 
-#[macro_use] extern crate proc_macro_hack;
-extern crate syn;
-extern crate hexf_parse;
+use proc_macro::TokenStream;
 
-proc_macro_expr_impl! {
-    /// Support function for `hexf32!` macro. Do not use directly.
-    #[doc(hidden)]
-    pub fn hexf32_impl(input: &str) -> String {
-        let lit = syn::parse_str::<syn::LitStr>(input).expect("hexf32! requires a single string literal");
-        match hexf_parse::parse_hexf32(&lit.value(), true) {
-            Ok(v) => format!("{:?}f32", v), // should keep the sign even for -0.0
-            Err(e) => panic!("hexf32! failed: {}", e),
-        }
+/// Expands to a `f32` value with given hexadecimal representation.
+///
+/// # Example
+///
+/// ```rust
+/// # use hexf::hexf32; fn main() {
+/// assert_eq!(hexf32!("0x1.99999ap-4"), 0.1f32);
+/// # }
+/// ```
+#[proc_macro]
+pub fn hexf32(input: TokenStream) -> TokenStream {
+    let lit = syn::parse_macro_input!(input as syn::LitStr);
+    match hexf_parse::parse_hexf32(&lit.value(), true) {
+        Ok(v) => format!("{:?}f32", v) // should keep the sign even for -0.0
+            .parse()
+            .expect("formatted a f32 literal"),
+        Err(e) => panic!("hexf32! failed: {}", e),
     }
 }
 
-proc_macro_expr_impl! {
-    /// Support function for `hexf64!` macro. Do not use directly.
-    #[doc(hidden)]
-    pub fn hexf64_impl(input: &str) -> String {
-        let lit = syn::parse_str::<syn::LitStr>(input).expect("hexf64! requires a single string literal");
-        match hexf_parse::parse_hexf64(&lit.value(), true) {
-            Ok(v) => format!("{:?}f64", v), // should keep the sign even for -0.0
-            Err(e) => panic!("hexf64! failed: {}", e),
-        }
+/// Expands to a `f64` value with given hexadecimal representation.
+///
+/// # Example
+///
+/// ```rust
+/// # use hexf::hexf64; fn main() {
+/// assert_eq!(hexf64!("0x1.999999999999ap-4"), 0.1f64);
+/// # }
+/// ```
+#[proc_macro]
+pub fn hexf64(input: TokenStream) -> TokenStream {
+    let lit = syn::parse_macro_input!(input as syn::LitStr);
+    match hexf_parse::parse_hexf64(&lit.value(), true) {
+        Ok(v) => format!("{:?}f64", v) // should keep the sign even for -0.0
+            .parse()
+            .expect("formatted a f64 literal"),
+        Err(e) => panic!("hexf64! failed: {}", e),
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -9,36 +9,5 @@
 //! # }
 //! ```
 
-#[macro_use] extern crate proc_macro_hack;
-#[macro_use] #[allow(unused_imports)] extern crate hexf_impl;
-extern crate hexf_parse;
-
-pub use hexf_parse::{ParseHexfError, parse_hexf32, parse_hexf64};
-#[doc(hidden)] pub use hexf_impl::*;
-
-proc_macro_expr_decl! {
-    /// Expands to a `f32` value with given hexadecimal representation.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// # #[macro_use] extern crate hexf; fn main() {
-    /// assert_eq!(hexf32!("0x1.99999ap-4"), 0.1f32);
-    /// # }
-    /// ```
-    hexf32! => hexf32_impl
-}
-
-proc_macro_expr_decl! {
-    /// Expands to a `f64` value with given hexadecimal representation.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// # #[macro_use] extern crate hexf; fn main() {
-    /// assert_eq!(hexf64!("0x1.999999999999ap-4"), 0.1f64);
-    /// # }
-    /// ```
-    hexf64! => hexf64_impl
-}
-
+pub use hexf_impl::{hexf32, hexf64};
+pub use hexf_parse::{parse_hexf32, parse_hexf64, ParseHexfError};
